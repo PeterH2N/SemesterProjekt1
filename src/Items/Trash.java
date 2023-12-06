f2b703225ec43b9ce16155bd9b455376f578e416
jacobrasmussen123 <144017516+jacobrasmussen123@users.noreply.github.com>
2023-12-06 11:19:40 +0100

    opdateret wastetype og tiføjet trashindex

---
+++ src/Items/Trash.java
@@ -0,0 +1,28 @@
+package Items;
+
+import java.util.*;
+import Items.*;
+//styre "pokedexet"
+public class Trash {
+    private Map<Integer, String> trashMap;
+
+    public Trash() {
+        trashMap = new HashMap<>();
+    }
+
+    public void addTrash(String trash) {
+        trashMap.put(WasteType.wasteTypes.get(trash).getIndex(), trash);
+    }
+
+    public String getTrash(int number) {
+        return trashMap.getOrDefault(number, null);
+    }
+    public void listAllTrash(){
+        System.out.println("Trash in the trashIndex");
+        for (String trash : trashMap.values()){
+            System.out.println("-------------------");
+            System.out.println(WasteType.wasteTypes.get(trash));
+            System.out.println("-------------------");
+        }
+    }
+}
